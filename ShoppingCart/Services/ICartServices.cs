085b72836851a5de6895bd0a1df7dc48637e7547
sandeepdewangan <sandeepinwork@gmail.com>
2026-07-08 12:16:22 +0530

    Shopping Cart App

---
+++ ShoppingCart/Services/ICartServices.cs
@@ -0,0 +1,13 @@
+﻿using ShoppingCart.Models;
+
+namespace ShoppingCart.Services
+{
+    public interface ICartServices
+    {
+        IList<Product> Cart { get; }
+        int Total { get; }
+        event Action OnChange;
+        void AddProduct(Product product);
+        void DeleteProduct(Product product);
+    }
+}
